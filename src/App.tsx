945c1f86972d0a4644cdfe0f74def305a37ef1d1
guilhermelins7 <guilherme7lins@gmail.com>
2025-05-11 18:59:53 -0300

    feat: criar botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Botao from "./components/Botao";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Botao />
+    </div>
+  );
 }
 
 export default App;
