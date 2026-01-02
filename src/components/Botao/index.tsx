04b58c6fe74ff935f0a325502715298a899bc4a1
guilhermelins7 <guilherme7lins@gmail.com>
2026-01-02 00:43:10 -0300

    feat: children prop para botao

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,7 @@
-import React from "react";
 import style from "./Botao.module.scss";
 
-class Botao extends React.Component {
-  // Função obrigatória para o componente de classe:
-  render() {
-    return <button className={style.botao}>Botão</button>;
-  }
+export default function Botao({ children }: {children: React.ReactNode}) {
+  return (
+    <button className={style.botao}>{children}</button>
+  );
 }
-
-export default Botao;
