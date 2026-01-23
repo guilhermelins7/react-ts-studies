740d1e6aaca084a5ded84aab4405986fa910d3c2
guilhermelins7 <guilherme7lins@gmail.com>
2026-01-22 21:51:00 -0300

    feat: Selecionar tempo com useEffect e ciclo de vida

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,13 +1,25 @@
 import Botao from '../Botao';
 import Relogio from './Relogio';
 import styles from './Cronometro.module.scss';
-import { tempoParaSegundos } from '../../common/utils/date';
+import { tempoParaSegundos } from '../../common/utils/time';
+import { ITarefa } from '../../types/ITarefas';
+import { useEffect, useState } from 'react';
 
-export function Cronometro() {
-    console.log('Conversão:', tempoParaSegundos("01:01:01"));
+interface Props {
+    selecionado: ITarefa | undefined
+}
+
+export function Cronometro({ selecionado }: Props) {
+    const [ tempo, setTempo ] = useState<number>();
+
+    useEffect(() => {
+        if(selecionado?.tempo) setTempo(tempoParaSegundos(selecionado.tempo));
+    }, [selecionado]);
+
     return(
         <div className={styles.cronometro}>
             <p className={styles.titulo} >Escolha um card e inicie o cronômetro</p>
+            <p>Tempo: {tempo}</p>
             <div className={styles.relogioWrapper} >
                 <Relogio/>
             </div>
